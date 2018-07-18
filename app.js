40337fed8c29230f4a52bbd2685083ba13ec8cd7
TaniaUdomsri <TaniaUdomsri@users.noreply.github.com>
2018-07-18 00:02:57 -0700

    Portfolio layout

--- app.js
+++ app.js
@@ -1,8 +1,67 @@
 import {MDCRipple} from '@material/ripple';
 import {MDCTextField} from '@material/textfield';
+import {MDCTopAppBar} from '@material/top-app-bar/index';
 
 const username = new MDCTextField(document.querySelector('.username'));
 const password = new MDCTextField(document.querySelector('.password'));
 
 new MDCRipple(document.querySelector('.cancel'));
 new MDCRipple(document.querySelector('.next'));
+
+
+// const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+// const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+var dynamicTabBar = window.dynamicTabBar = new mdc.tabs.MDCTabBar(document.querySelector('#dynamic-tab-bar'));
+var dots = document.querySelector('.dots');
+var panels = document.querySelector('.panels');
+
+dynamicTabBar.tabs.forEach(function(tab) {
+  tab.preventDefaultOnClick = true;
+});
+
+function updateDot(index) {
+  var activeDot = dots.querySelector('.dot.active');
+  if (activeDot) {
+    activeDot.classList.remove('active');
+  }
+  var newActiveDot = dots.querySelector('.dot:nth-child(' + (index + 1) + ')');
+  if (newActiveDot) {
+    newActiveDot.classList.add('active');
+  }
+}
+
+function updatePanel(index) {
+  var activePanel = panels.querySelector('.panel.active');
+  if (activePanel) {
+    activePanel.classList.remove('active');
+  }
+  var newActivePanel = panels.querySelector('.panel:nth-child(' + (index + 1) + ')');
+  if (newActivePanel) {
+    newActivePanel.classList.add('active');
+  }
+}
+
+dynamicTabBar.listen('MDCTabBar:change', function ({detail: tabs}) {
+  var nthChildIndex = tabs.activeTabIndex;
+
+  updatePanel(nthChildIndex);
+  updateDot(nthChildIndex);
+});
+
+dots.addEventListener('click', function (evt) {
+  if (!evt.target.classList.contains('dot')) {
+    return;
+  }
+
+  evt.preventDefault();
+
+  var dotIndex = [].slice.call(dots.querySelectorAll('.dot')).indexOf(evt.target);
+
+  if (dotIndex >= 0) {
+    dynamicTabBar.activeTabIndex = dotIndex;
+  }
+
+  updatePanel(dotIndex);
+  updateDot(dotIndex);
+})
