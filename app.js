7e219cac453a5919b074eb1cd10bfd8284cb92da
TaniaUdomsri <TaniaUdomsri@users.noreply.github.com>
2018-08-06 15:38:58 -0700

    Make footer component and clean up files

--- app.js
+++ app.js
@@ -1,57 +1,27 @@
-import {MDCRipple} from '@material/ripple';
-import {MDCTextField} from '@material/textfield';
-import {MDCTopAppBar} from '@material/top-app-bar/index';
+import regeneratorRuntime from "regenerator-runtime";
 
-var dynamicTabBar = window.dynamicTabBar = new mdc.tabs.MDCTabBar(document.querySelector('#dynamic-tab-bar'));
-var dots = document.querySelector('.dots');
-var panels = document.querySelector('.panels');
+$(function() {
+  
+  // Top nav menu
+  $('body').on('click', '.top-menu-item', function() {
+    let menu = $(this).attr('menu');
+    $.scrollTo('#' + menu, 500, {offset: {top:0, left:0} });
+  })
 
-dynamicTabBar.tabs.forEach(function(tab) {
-  tab.preventDefaultOnClick = true;
+  // Portfolio cards
+  $('body').on('click', '.portfolio-card', function() {
+    let port = $(this).attr('portfolio');
+    window.location = '/' + port + '.html';
+  });
+
+  var loadHtmlContent = (fetchUrl, contentId) => {
+    $.ajax({
+      url: fetchUrl,
+      success: function(data){
+        document.getElementById(contentId).innerHTML = data;
+      }
+    });
+  };
+
+  loadHtmlContent('./components/footer.html', 'footer');
 });
-
-function updateDot(index) {
-  var activeDot = dots.querySelector('.dot.active');
-  if (activeDot) {
-    activeDot.classList.remove('active');
-  }
-  var newActiveDot = dots.querySelector('.dot:nth-child(' + (index + 1) + ')');
-  if (newActiveDot) {
-    newActiveDot.classList.add('active');
-  }
-}
-
-function updatePanel(index) {
-  var activePanel = panels.querySelector('.panel.active');
-  if (activePanel) {
-    activePanel.classList.remove('active');
-  }
-  var newActivePanel = panels.querySelector('.panel:nth-child(' + (index + 1) + ')');
-  if (newActivePanel) {
-    newActivePanel.classList.add('active');
-  }
-}
-
-dynamicTabBar.listen('MDCTabBar:change', function ({detail: tabs}) {
-  var nthChildIndex = tabs.activeTabIndex;
-
-  updatePanel(nthChildIndex);
-  updateDot(nthChildIndex);
-});
-
-dots.addEventListener('click', function (evt) {
-  if (!evt.target.classList.contains('dot')) {
-    return;
-  }
-
-  evt.preventDefault();
-
-  var dotIndex = [].slice.call(dots.querySelectorAll('.dot')).indexOf(evt.target);
-
-  if (dotIndex >= 0) {
-    dynamicTabBar.activeTabIndex = dotIndex;
-  }
-
-  updatePanel(dotIndex);
-  updateDot(dotIndex);
-})
